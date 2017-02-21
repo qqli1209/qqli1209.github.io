7992ae3ee8cd90f3d71ffa4706686ed8a4d37839
qqli1209 <lqq201194@163.com>
2017-02-21 20:04:47 +0800

    add leafs animation while hover on logo

--- js/header.js
+++ js/header.js
@@ -6,5 +6,22 @@ $(function(){
 	},function(){
 		//remove the parent li selected class
 		$(this).parent().parent().parent().find("a").first().removeClass("selected");
+	});
+
+	// hover on header-log, the foregroung and background move in opposite direction
+	var back = $("div.header");
+	var fore = $("div.foreground");
+	var skew = 50;
+	$(".header-logo h1 a").hover(function(){
+		var b = back.css("backgroundPosition").split(" ");
+		var f = fore.css("backgroundPosition").split(" ");
+		var backNewPos = (parseInt(b[0])+skew) + "%";
+		var foreNewPos = (parseInt(f[0])-skew) + "%";
+		//alert(newPosition);
+		if(!back.is(":animated")){
+			back.animate({"background-position-x":backNewPos},8000);
+			fore.animate({"background-position-x":foreNewPos},8000);
+		}
 	})
+
 })
