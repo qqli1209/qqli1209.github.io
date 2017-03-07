6c156234b3d1f98cd4f3d423fc27a74fc73148ea
qqli1209 <lqq201194@163.com>
2017-03-07 10:40:48 +0800

    pageNav前进，后退按钮

--- js/pageNav.js
+++ js/pageNav.js
@@ -1,15 +1,26 @@
 
 
 const ARTICLE_PER_PAGE = 8;
-//pageLink.eq(1).click();
+
+//按用户点击pageLink,显示网页文章articles
+function showArticle(articles,pageLink){
+    var len = pageLink.length;
+    for(let j=1; j<len-1; j++){
+        pageLink.eq(j).click(function(){
+            $(this).addClass("flag").siblings().removeClass("flag");
+            articles.hide();
+            for(var page = ARTICLE_PER_PAGE*(j-1);page< ARTICLE_PER_PAGE*j; page++){
+                articles.eq(page).show();
+            }
+        });
+    }
+}
+
 
 $(function(){
-    //
-    //var articles = $("#myArticles").find("article");
-    //console.log(articles.length);
-    //console.log(ARTICLE_PER_PAGE);  //8
+    var articles = $("article");
     var pageLink = $("#pageNav").find("li");
-    
+    var pageA = $("#pageNav");
     var len = pageLink.length;
     //console.log(len);
     
@@ -20,25 +31,36 @@ $(function(){
             $(this).siblings().find("a").removeClass("pageSelected");
         });
     }
-    
-    pageLink.eq(1).click(function(){
-        $("article").show();
-        $("article:gt(7)").hide();
+    showArticle(articles,pageLink);
+
+    //后退
+    pageLink.eq(0).click(function(){
+        //现在选中哪个
+        var curPage = $("li.flag");
+        //console.log(curPage.html());
+        console.log(curPage.index());
+        var index = curPage.index();
+        if(index>1){
+            pageLink.eq(index-1).click();
+        } else {
+            pageLink.eq(1).click();
+        } 
     });
-    pageLink.eq(2).click(function(){
-        $("article").show();
-        $("article:gt(15)").hide();
-        $("article:lt(8)").hide();
+
+    //前进
+    pageLink.eq(len-1).click(function(){
+        //现在选中哪个
+        var curPage = $("li.flag");
+        //console.log(curPage.html());
+        console.log(curPage.index());
+        var index = curPage.index();
+        if(index<len-2){
+            pageLink.eq(index+1).click();
+        } else {
+            pageLink.eq(len-1).click();
+        }
     });
-    pageLink.eq(3).click(function(){
-        $("article").show();
-        $("article:gt(23)").hide();
-        $("article:lt(16)").hide();
-    });
-    pageLink.eq(4).click(function(){
-        $("article").show();
-        $("article:gt(31)").hide();
-        $("article:lt(24)").hide();
-    });
+
+    //进入页面默认点击page1
     pageLink.eq(1).click();
 });
