7d7dfd0f572eb38910f24f673ffa843311196f23
qqli1209 <lqq201194@163.com>
2017-03-12 12:24:51 +0800

    work on nav bugs

--- js/pageNav.js
+++ js/pageNav.js
@@ -63,4 +63,38 @@ $(function(){
 
     //进入页面默认点击page1
     pageLink.eq(1).click();
+
+
+    //显示不同的文章
+    var article = $("article");
+    var article_js = $("article.js");
+    var article_html = $("article.html");
+    var article_css = $("article.css");
+    var article_eassy = $("article.eassy");
+
+    $(".html-article").click(function(){
+        event.stopPropagation();
+        article.hide();
+        article_html.show();
+    });
+    $(".js-article").click(function(){
+        event.stopPropagation();
+        article.hide();
+        article_js.show();
+    });
+    $(".css-article").click(function(){
+        event.stopPropagation();
+        article.hide();
+        article_css.show();
+    });
+    $(".eassy-article").click(function(){
+        event.stopPropagation();
+        article.hide();
+        article_eassy.show();
+    });
+    $(".all-articles").click(function(){
+        pageLink.eq(1).click();
+    });
+
+
 });
