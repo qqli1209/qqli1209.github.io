9af94172a8366ee6c9ee3af93a0b972628c061d1
qqli1209 <lqq201194@163.com>
2017-03-12 20:34:36 +0800

    show big image whilt hover on thumbnail

--- js/showImage.js
+++ js/showImage.js
@@ -2,16 +2,30 @@
 
 
 
-/*
-$(function(){
 
-	$(".flex-column>img").hover(function(e){
-		$(this).next().removeClass("hide");
-	},function(e){
-	$(this).next().addClass("hide");
+$(function(){
+	var x = 10;
+	var y = 20;
+	$(".flex-column img").mouseover(function(e){
+		this.myTitle = this.title;
+		this.title = "";
+		var bigimg = "<div id = 'bigimg'><img src = '" + this.myTitle + "' alt = '预览图'/>"+"<\/div>";
+		$("body").append(bigimg);
+		$("#bigimg").css({
+			"top" : (e.pageY+y)+"px",
+			"left" : (e.pageX+x)+"px"}).show("fast");
+			console.log($("#bigimg"));
+	}).mouseout(function(){
+		this.title = this.myTitle;
+		$("#bigimg").remove();
+	}).mousemove(function(e){
+		$("#bigimg").css({
+			"top" : (e.pageY+y)+"px",
+			"left" : (e.pageX+x)+"px"});
+	});
+
 });
 
-*/
 
 
 
